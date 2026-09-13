86c5b1b813d231a819617f5a25f711b74b85cb92
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-09-13 17:57:09 +0100

    update

--- answer.sql
+++ answer.sql
@@ -7,25 +7,15 @@
 -- Daily average users before and after the feature change.
 -- Feature change date: 2018-06-02
 
-WITH daily_users AS (
-    SELECT
-        date(login_timestamp, 'unixepoch') AS login_date,
-        COUNT(DISTINCT user_id) AS users
-    FROM login_history
-    GROUP BY date(login_timestamp, 'unixepoch')
-)
 SELECT
-    CASE
-        WHEN login_date < '2018-06-02' THEN 'before'
-        ELSE 'after'
-    END AS period,
-    AVG(users) AS daily_average_users
-FROM daily_users
-GROUP BY period;
+    date(login_timestamp, 'unixepoch') AS login_date,
+    COUNT(DISTINCT user_id) AS daily_active_users
+FROM login_history
+GROUP BY date(login_timestamp, 'unixepoch')
+ORDER BY login_date;
 
 
 -- PART 2: Create a SQL query that indicates the number of status changes by card
--- Number of status changes by card.
 
 SELECT
     cardID AS card_id,
